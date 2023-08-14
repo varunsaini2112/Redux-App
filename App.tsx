8014bcaed3bab60b553b29f24cc8763024704c4b
varunsaini21 <varun.saini@squarehealth.com>
2023-08-14 22:56:35 +0530

    Project setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+const App = () => {
+	return (
+		<NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+	);
+};
+
+export default App;
