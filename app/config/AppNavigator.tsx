19f4b5146290111ebab9533a7197288c4947ba5b
varunsaini21 <varun.saini@squarehealth.com>
2023-08-15 02:15:27 +0530

    type added for stack

--- app/config/AppNavigator.tsx
+++ app/config/AppNavigator.tsx
@@ -2,8 +2,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "../screens";
+import { RootStackParamList } from "../types";
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
 	return (
