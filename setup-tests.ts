32ac8a622ab88c4540f9a2feb03cc841600cfccb
varunsaini21 <varun.saini@squarehealth.com>
2023-08-15 14:05:13 +0530

    setup test updated

--- setup-tests.ts
+++ setup-tests.ts
@@ -2,3 +2,7 @@ import "@testing-library/jest-native/extend-expect";
 import "react-native-gesture-handler/jestSetup";
 
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+beforeEach(() => {
+	jest.resetAllMocks();
+});
