d1d35185241b7073818bed301353a14e8205bae5
varunsaini21 <varun.saini@squarehealth.com>
2023-08-25 16:32:29 +0530

    button redundant style removed on count styles file

--- app/screens/Count/Count.styles.ts
+++ app/screens/Count/Count.styles.ts
@@ -1,17 +1,9 @@
 import { StyleSheet } from "react-native";
-import { Padding, colors } from "../../config/appStyles";
 
 const styles = StyleSheet.create({
 	mainContainer: {
 		flex: 1,
 		justifyContent: "center",
-		alignItems: "center"
-	},
-	button: {
-		padding: Padding.xl,
-		backgroundColor: colors.primary,
-		margin: Padding.xl,
-		borderRadius: Padding.xl,
 		alignItems: "center"
 	}
 });
